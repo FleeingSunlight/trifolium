e9273552a741c1dc9b53f233eb3e65a50f429a9f
FleeingSunlight <gh@fleeingsunlight.dev>
2021-01-17 15:58:32 +0700

    feat(infra): use EJS templating

--- infra/cluster.ts
+++ infra/cluster.ts
@@ -3,23 +3,82 @@ import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
 import * as config from "./config";
 import * as vpc from "./vpc";
+import * as fs from "fs";
+import * as path from "path";
+import * as ejs from "ejs";
 
-export const cluster = new gcp.container.Cluster("trifolium", {
-  initialNodeCount: 1,
-  removeDefaultNodePool: true,
-  minMasterVersion: config.masterVersion,
-  masterAuth: {
-    username: config.username,
-    password: config.password,
+export const cluster = new gcp.container.Cluster(
+  "trifolium",
+  {
+    initialNodeCount: 1,
+    removeDefaultNodePool: true,
+    location: vpc.subnetwork.region + "-a",
+    minMasterVersion: config.masterVersion,
+    masterAuth: {
+      username: config.username,
+      password: config.password,
+    },
+    privateClusterConfig: {
+      enablePrivateEndpoint: true,
+      enablePrivateNodes: true,
+      masterIpv4CidrBlock: "172.16.0.0/28",
+    },
+    network: vpc.network,
+    subnetwork: vpc.subnetwork,
   },
-  privateClusterConfig: {
-    enablePrivateEndpoint: true,
-    enablePrivateNodes: true,
-    masterIpv4CidrBlock: "172.16.0.0/28",
+  {
+    dependsOn: vpc.subnetwork,
+    parent: vpc.subnetwork,
+  }
+);
+
+export const nodePool = new gcp.container.NodePool(
+  "trifolium-node-pool",
+  {
+    cluster: cluster.name,
+    initialNodeCount: config.nodeCount,
+    location: cluster.location,
+    nodeConfig: {
+      preemptible: true,
+      machineType: config.nodeMachineType,
+      oauthScopes: [
+        "https://www.googleapis.com/auth/compute",
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+      ],
+    },
+    version: config.masterVersion,
+    management: {
+      autoRepair: true,
+    },
   },
-  network: vpc.network,
-  subnetwork: vpc.subnetwork,
-}, {
-  dependsOn: vpc.subnetwork,
-  parent: vpc.subnetwork
-});
+  {
+    dependsOn: cluster,
+    parent: cluster,
+  }
+);
+
+export const kubeconfig = pulumi
+  .all([cluster.name, cluster.endpoint, cluster.masterAuth])
+  .apply(([name, endpoint, auth]) => {
+    const context = gcp.config.project + "_" + gcp.config.zone + "_" + name;
+    return ejs.render(
+      fs.readFileSync(path.resolve(__dirname, "gke-template.yaml"), "utf-8"),
+      {
+        clusterCertificate: auth.clusterCaCertificate,
+        server: "https://" + endpoint,
+        context: context,
+      }
+    );
+  });
+
+export const provider = new k8s.Provider(
+  "trifolium-k8s",
+  {
+    kubeconfig,
+  },
+  {
+    dependsOn: nodePool,
+  }
+);
