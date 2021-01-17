511c153175366a4bbaff15d32111e622cc029fc6
FleeingSunlight <gh@fleeingsunlight.dev>
2021-01-17 15:47:05 +0700

    feat(infra): add cluster.ts + vpc.ts

---
+++ infra/cluster.ts
@@ -0,0 +1,22 @@
+import * as gcp from "@pulumi/gcp";
+import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+import * as config from "./config";
+import * as vpc from "./vpc";
+
+export const cluster = new gcp.container.Cluster("trifolium", {
+  initialNodeCount: 1,
+  removeDefaultNodePool: true,
+  minMasterVersion: config.masterVersion,
+  masterAuth: {
+    username: config.username,
+    password: config.password,
+  },
+  privateClusterConfig: {
+    enablePrivateEndpoint: true,
+    enablePrivateNodes: true,
+    masterIpv4CidrBlock: "172.16.0.0/28",
+  },
+  network: vpc.network,
+  subnetwork: vpc.subnetwork,
+});
---
+++ infra/vpc.ts
@@ -0,0 +1,12 @@
+import * as gcp from "@pulumi/gcp";
+import * as pulumi from "@pulumi/pulumi";
+
+export const network = new gcp.compute.Network("trifolium-network", {
+  autoCreateSubnetworks: false,
+});
+
+export const subnetwork = new gcp.compute.Subnetwork("trifolium-subnetwork", {
+  ipCidrRange: "10.2.0.0/16",
+  region: "asia-southeast1",
+  network: network.selfLink,
+});
